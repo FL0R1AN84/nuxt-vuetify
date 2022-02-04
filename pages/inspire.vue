a688ddc4e9e265e5e877d45d68d23b545f3aadd4
Florian Wächter <system@gammagate.de>
2022-02-04 20:42:31 +0100

    I am so funny

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -6,7 +6,9 @@
         &#8220;First, solve the problem. Then, write the code.&#8221;
         <footer>
           <small>
-            <em>&mdash;John Johnson</em>
+            <em>(╯°□°）╯︵ ┻━┻)</em>
+            <br>
+            <em>ᕕ( ᐛ )ᕗ</em>
           </small>
         </footer>
       </blockquote>
